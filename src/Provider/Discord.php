wohali/oauth2-discord-new

commit f34be83cbc88670405c4e63d27cf1003aa858089

Migrate v6 to v9 (#39)
Migration from API v6 to API v9

src/Provider/Discord.php
@@ -28,7 +28,7 @@ class Discord extends AbstractProvider
      *
      * @var string
      */
-    public $apiDomain = 'https://discord.com/api';
+    public $apiDomain = 'https://discord.com/api/v9';
 
     /**
      * Get authorization URL to begin OAuth flow

commit 2df4d2a882e04c749880797704e4bde8f00ea1d9

Update OAuth2 base authorization URL (#44)
* Update OAuth2 base authorization URL
* Fix authorization and access token tests

This has been caused by #39 which did not appropriately update
the authorization and access token URL tests in the same way as #33

src/Provider/Discord.php
@@ -26,7 +26,14 @@ class Discord extends AbstractProvider
     use BearerAuthorizationTrait;
 
     /**
-     * API Domain
+     * Default host
+     *
+     * @var string
+     */
+    public $host = 'https://discord.com';
+
+    /**
+     * API domain
      *
      * @var string
      */
@@ -39,7 +46,7 @@ class Discord extends AbstractProvider
      */
     public function getBaseAuthorizationUrl()
     {
-        return $this->apiDomain.'/oauth2/authorize';
+        return $this->host.'/oauth2/authorize';
     }
 
     /**

test/src/Provider/DiscordTest.php
@@ -54,7 +54,7 @@ class DiscordTest extends \PHPUnit\Framework\TestCase
         $url = $this->provider->getAuthorizationUrl();
         $uri = parse_url($url);
 
-        $this->assertEquals('/api/oauth2/authorize', $uri['path']);
+        $this->assertEquals('/oauth2/authorize', $uri['path']);
     }
 
     public function testGetBaseAccessTokenUrl()
@@ -64,7 +64,7 @@ class DiscordTest extends \PHPUnit\Framework\TestCase
         $url = $this->provider->getBaseAccessTokenUrl($params);
         $uri = parse_url($url);
 
-        $this->assertEquals('/api/oauth2/token', $uri['path']);
+        $this->assertEquals('/api/v9/oauth2/token', $uri['path']);
     }
 
     public function testGetAccessToken()